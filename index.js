8befe7e42ddae653c9a11b5db1bd0f9aa27efed4
serena costantine <serenacostantine05@icloud.com>
2024-09-04 17:52:06 +0300

    make a select

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function getStudents()
-{
+function getStudents() {
     fetch('https://session7-2.onrender.com/students')
-      .then(response => response.json())
-      .then(json => console.log(json))
+        .then(response => response.json())
+        .then(json => {
+            var select_users = document.getElementById('select_users');
+            json.forEach(st => {
+                var opt = document.createElement('option');
+                opt.innerHTML = st.name;
+                select_users.appendChild(opt);
+            });
+        });
 }
 
 getStudents();
